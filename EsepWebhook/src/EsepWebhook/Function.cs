bc0a532541f0a271de05063d4d58de6332512c48
jenny-xie <58479838+jenny-xie@users.noreply.github.com>
2022-10-14 22:18:21 -0400

    webhook

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -23,7 +23,7 @@ public class Function
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
 
         var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, "https://hooks.slack.com/services/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")
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, "{do not check in this url}")
         {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
         };
